60264b7c5a4c563772a059f4dc0a81522290b320
Abdullah Diaa <abdullah.diaa@gmail.com>
2015-10-28 12:30:14 +0200

    Create new Push handler && Bug fixes (Android)
    - Fixes #13 use runOnUiThread
    - Add registrered/ Received notification/ Opened notification events.
    - Fixes #12 use registered event to get the token with first open

--- src/android/com/pushbots/plugin/PushbotsPlugin.java
+++ src/android/com/pushbots/plugin/PushbotsPlugin.java
@@ -1,164 +1,220 @@
 package com.pushbots.plugin;
 
+import com.pushbots.logapp.MainActivity;
 import com.pushbots.push.Pushbots;
-
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
 import com.pushbots.push.utils.Log;
+import com.pushbots.push.utils.PBConstants;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 public class PushbotsPlugin extends CordovaPlugin {
 
+	private static CallbackContext _callbackContext;
 	public static CordovaWebView gwebView;
 	public static Activity mActivity;
-	public static JSONObject pendingMessage;
-	public static String pendingFunctionName;
 
 	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext cb) throws JSONException {
+	public boolean execute(final String action,final JSONArray args, final CallbackContext cb) throws JSONException {
 
 		gwebView = this.webView;
 		mActivity = cordova.getActivity();
 
-		if (action.equals("initializeWithAppId")) {
-			this.initialize(cb, args);
+		if (action.equals("initialize")) {
+			_initialize(args, cb);
 			return true;
 		}
 
-		if (action.equals("initializeWithAppIdAndSenderId")) {
-			this.initializeSender(cb, args);
+		if (action.equals("updateAlias")) {
+			_updateAlias(args, cb);
 			return true;
-		}
-
-		if (action.equals("setAlias")) {
-			String alias;
-			try {
-				alias = args.getString(0);
-
-				if (alias.equals("")) {
-					alias = null;
-				}
-
-				Pushbots.sharedInstance().setAlias(alias);
-				cb.success("Alias Updated Successfully.");
-			} catch (JSONException e) {
-				cb.error("Error updating Alias.");
-				e.printStackTrace();
-				return false;
-			}
 		}
 
 		if (action.equals("tag")) {
-			String tag;
-			try {
-				tag = args.getString(0);
-				if (tag.equals("")) {
-					tag = null;
-				}
-				Pushbots.sharedInstance().tag(tag);
-				cb.success("Tag Updated Successfully.");
-			} catch (JSONException e) {
-				cb.error("Error updating tag.");
-				e.printStackTrace();
-				return false;
-			}
+			_tag(args, cb);
+			return true;
 		}
 
 		if (action.equals("untag")) {
-			String tag;
-			try {
-				tag = args.getString(0);
-				if (tag.equals("")) {
-					tag = null;
-				}
-				Pushbots.sharedInstance().untag(tag);
-				cb.success("Tag Updated Successfully.");
-			} catch (JSONException e) {
-				cb.error("Error updating tag.");
-				e.printStackTrace();
-				return false;
-			}
+			_untag(args, cb);
+			return true;
 		}
 
 		if (action.equals("debug")) {
-			Boolean debug;
-			try {
-				debug = args.getBoolean(0);
-				Pushbots.sharedInstance().debug(debug);
-				cb.success("Device set as debugging device.");
-			} catch (JSONException e) {
-				cb.error("Error updating debug tag.");
-				e.printStackTrace();
-				return false;
-			}
+			_debug(args, cb);
+			return true;
 		}
 
-		//Unregister device from Pushbots
 		if (action.equals("unregister")) {
-			Pushbots.sharedInstance().unRegister();
-			cb.success("Device unregistered Successfully.");
+			_unregister(args, cb);
+			return true;
 		}
-		
-		//Get Device registration ID
-		if (action.equals("getToken"))
-		{
+
+		if (action.equals("getRegistrationId")){
 			String registrationID = Pushbots.sharedInstance().regID();
-			Log.d("Getting device Token: " +  registrationID);
 			cb.success(registrationID);
 			return true;
 		}
-		
+
 		return true;
 	}
 
-	public static void sendJavascript(  String funName,  JSONObject _json )
-	{
-		
-		String _d =  "javascript:PushbotsPlugin."+funName+"(" + _json.toString() + ")";
-		if(gwebView !=null){
-			gwebView.sendJavascript( _d );
-		}else{
-			pendingMessage = _json;
-			pendingFunctionName= funName;
-		}
+	private void _initialize(JSONArray args, CallbackContext cb) throws JSONException {
 
-	}
-	public static void sendMessage( String funName, JSONObject _json)
-	{
-		pendingMessage = _json;
-		pendingFunctionName = funName;
-	}
+		final String applicationId = args.getString(0);
+		JSONObject options = null;
+		options = args.getJSONObject(1).getJSONObject("android");
+		final String senderId = options.getString("sender_id");
 
-	private void initialize(final CallbackContext cb, final JSONArray args) {
-		gwebView = this.webView;
-		if(pendingMessage != null){
-			sendJavascript(pendingFunctionName, pendingMessage);
-			pendingMessage = null;
-		}
-		Pushbots.sharedInstance().init(cordova.getActivity());
-		Pushbots.sharedInstance().setCustomHandler(Receiver.class);
-		cb.success();
-	}
-	
-	private void initializeSender(final CallbackContext cb, final JSONArray args) {
-		try {
-			gwebView = this.webView;
-			if(pendingMessage != null){
-				sendJavascript(pendingFunctionName, pendingMessage);
-				pendingMessage = null;
+		_callbackContext = cb;
+
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				// Initialize Pushbots Plugin
+				Pushbots.sharedInstance().init(cordova.getActivity(), applicationId, senderId, "DEBUG");
+				Pushbots.sharedInstance().setCustomHandler(PushHandler.class);
+				
+				// Check for attached Push data on opening the App from a notification
+                Bundle bundle = cordova.getActivity().getIntent().getBundleExtra("pushData");
+                if(bundle != null){
+                    try{
+                        JSONObject json = new JSONObject();
+                        Set<String> keys = bundle.keySet();
+                        Iterator<String> it = keys.iterator();
+                        while (it.hasNext()) {
+                            String key = it.next();
+                            json.put(key, bundle.get(key));
+                        }
+                        sendSuccessData("opened", json);
+                    }catch (NullPointerException e){
+                        Log.e("Null");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+				noResult();
 			}
-			Pushbots.sharedInstance().init(cordova.getActivity(), args.getString(0), args.getString(1), "DEBUG");
-			Pushbots.sharedInstance().setCustomHandler(Receiver.class);
-			cb.success();
-		} catch (JSONException e) {
-			cb.error("Error initializing the app.");
-			e.printStackTrace();
+		});
+	}
+
+	private void _updateAlias(JSONArray args, CallbackContext cb) throws JSONException {
+
+		final String alias = args.getString(0);
+
+		_callbackContext = cb;
+
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Pushbots.sharedInstance().setAlias(alias);
+				noResult();
+			}
+		});
+	}
+
+	private void _tag(JSONArray args, CallbackContext cb) throws JSONException {
+
+		final String tag = args.getString(0);
+
+		_callbackContext = cb;
+
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Pushbots.sharedInstance().tag(tag);
+				noResult();
+			}
+		});
+	}
+
+	private void _untag(JSONArray args, CallbackContext cb) throws JSONException {
+
+		final String tag = args.getString(0);
+
+		_callbackContext = cb;
+
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Pushbots.sharedInstance().untag(tag);
+				noResult();
+			}
+		});
+	}
+
+	private void _debug(JSONArray args, CallbackContext cb) throws JSONException {
+
+		final Boolean debug = args.getBoolean(0);
+
+		_callbackContext = cb;
+
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Pushbots.sharedInstance().debug(debug);
+				noResult();
+			}
+		});
+	}
+
+	private void _unregister(JSONArray args, CallbackContext cb) throws JSONException {
+
+		_callbackContext = cb;
+
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Pushbots.sharedInstance().unRegister();
+				noResult();
+			}
+		});
+	}
+
+	public static void sendSuccessData(String type, Object data) {
+
+		if (type != null && data != null) {
+			JSONObject result = new JSONObject();
+			try {
+				result.put("type", type);
+				result.put("data", data);
+				PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, result);
+				pluginResult.setKeepCallback(true);
+				if (_callbackContext != null) {
+					Log.d("Sending success result: " + pluginResult.getMessage());
+					_callbackContext.sendPluginResult(pluginResult);
+				}
+			} catch (JSONException e) {
+				Log.e("could not serialize result for callback");
+			}
 		}
 	}
-	
+
+	public static void fail(String message) {
+		PluginResult result = new PluginResult(PluginResult.Status.ERROR, message);
+		result.setKeepCallback(true);
+		_callbackContext.sendPluginResult(result);
+	}
+
+	private static void noResult(){
+		PluginResult result = new PluginResult(PluginResult.Status.OK, "");
+		result.setKeepCallback(true);
+		_callbackContext.sendPluginResult(result);
+	}
+
 }
